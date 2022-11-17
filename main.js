2abe8ab5f14266f04226012f13a096e7d0d51ccb
CannelleJP <isaibmezghiche@edenschool.fr>
2022-11-17 17:15:27 +0100

    no lack

---
+++ main.js
@@ -0,0 +1,52 @@
+/*for (let i = 0; i <= 10; i++) {
+ console.log(i);  
+}*/
+
+
+//--------------------------------------------
+
+/* for (let i = 1; i <= 100; i+=2) {
+ console.log(i);
+}*/
+
+
+
+//----------------------------------------------
+ /*let n = 1
+for (let i = 0; i < 10; i++) {
+
+  console.log(n * 7)
+  n++
+}*/
+
+
+
+//----------------------------------------------
+
+/*function multi(y) {
+  for (let x = 0; x <= 10; x++) {
+ console.log(x * y)
+  
+}
+}
+
+console.log(multi(5));*/
+
+
+//--------------------------------------------------------
+/*
+function somme(n) {
+  let total = 0
+  for (let i = 0; i <= n ; i++) {
+  total += i
+    
+  }
+  return total;
+}
+
+console.log(somme(10));*/
+
+for (let i = 0; i < 10; i++) {
+ 
+  
+}
